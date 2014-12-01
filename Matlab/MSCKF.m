1b88da506623c033c1bcfb4b7bc823f3c6a19ed1
Valentin Peretroukhin <valentinp@gmail.com>
2014-12-01 14:00:29 -0500

    Added state definitions

--- Matlab/MSCKF.m
+++ Matlab/MSCKF.m
@@ -1,1 +1,21 @@
 addpath('utils');
+load('dataset3.mat')
+
+%Set up the camera parameters
+camera.c_u = cu;
+camera.c_v = cv;
+camera.f_u = fu;
+camera.f_v = fv;
+camera.b = b;
+
+% Notation: X_sub_super
+
+imuState.q_iG = [zeros(3,1); 1];  %Global to IMU rotation quaternion
+imuState.p_G = zeros(3,1); %IMU Position in the Global frame
+imuState.b_g = zeros(3,1); %Gyro bias
+imuState.b_v = zeros(3,1); %Velocity bias
+
+camState.q_CG = [zeros(3,1); 1];
+camState.p_C_G = zeros(3,1);
+
+%msckfState = {imuState, camState1, camState2,....}
