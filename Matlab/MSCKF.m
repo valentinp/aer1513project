de912ae7385e4652e40e5d6a2133770509f941f6
Lee Clement <clement.leopold@gmail.com>
2014-12-01 18:26:35 -0500

    Added propagation functions and jacobians

--- Matlab/MSCKF.m
+++ Matlab/MSCKF.m
@@ -2,31 +2,36 @@ addpath('utils');
 load('dataset3.mat')
 
 %Set up the camera parameters
-camera.c_u = cu;
-camera.c_v = cv;
-camera.f_u = fu;
-camera.f_v = fv;
-camera.b = b;
-camera.q_CI  = C_c_v;
-camera.p_C_I = rho_v_c_v;
+camera.c_u      = cu;
+camera.c_v      = cv;
+camera.f_u      = fu;
+camera.f_v      = fv;
+camera.b        = b;
+camera.q_CI     = C_c_v;
+camera.p_C_I    = rho_v_c_v;
 
 % Notation: X_sub_super, q_FromTo, p_ofWhat_expressedInWhatFrame
 
 
-imuState.q_IG{1} = [zeros(3,1); 1];    %Global to IMU rotation quaternion
-imuState.p_G{1}  = zeros(3,1);         %IMU Position in the Global frame
-imuState.b_g{1}  = zeros(3,1);         %Gyro bias
-imuState.b_v{1}  = zeros(3,1);         %Velocity bias
-
-imuErrorState.dTheta{1} = zeros(3,1);
-imuErrorState.dp = zeros(3,1);
-imuErrorState.db_g{1} = zeros(3,1);
-imuErrorState.db_v{1} = zeros(3,1);
+imuState.q_IG{1}  = [zeros(3,1); 1];    %Global to IMU rotation quaternion
+imuState.p_I_G{1} = zeros(3,1);         %IMU Position in the Global frame
+imuState.b_g{1}   = zeros(3,1);         %Gyro bias
+imuState.b_v{1}   = zeros(3,1);         %Velocity bias
+imuState.covar{1} = zeros(12,12);       %IMU state covariance
 
 camState.q_CG{1}  = [zeros(3,1); 1];
 camState.p_C_G{1} = zeros(3,1);
 
-%msckfState = {imuState, camState1, camState2,....}
+%msckfState = {imuState, camState1, camState2, ...}
+
+% Measurements as cells
+dT = [0, diff(t)];
+for cellIdx = 1:numel(t)    
+    measurements.dT{cellIdx}    = dT(cellIdx);     % sampling times
+    measurements.y{k}           = y_k_j(1:2,k,:);  % left camera only
+    measurements.omega{k}       = w_vk_vk_i(:,k);  % ang vel
+    measurements.v{k}           = v_vk_vk_i(:,k);  % lin vel
+end
 
 Nmax = 50;
 
