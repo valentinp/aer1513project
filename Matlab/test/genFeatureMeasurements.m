8adfedb30e2bab03ed0086ba7fb24adb50b96bec
Valentin Peretroukhin <valentinp@gmail.com>
2014-12-05 12:18:28 -0500

    Added rotmat to quaternion helper and more GN testing

--- Matlab/test/genFeatureMeasurements.m
+++ Matlab/test/genFeatureMeasurements.m
@@ -12,21 +12,22 @@ for step_i = 1:size(T_wCam_GT,3)
         landmarks_cam = homo2cart(inv(T_wCam_GT(:,:,step_i))*cart2homo(landmarks_w));
         pixels = homo2cart(K*landmarks_cam);
         
-        %Determine which landmarks are viewable and find their ids
-        viewableLandmarksIdx = pixels(1,:) > 0 & pixels(1,:) < camRes(2) & pixels(2,:) > 0 & pixels(2,:) < camRes(1);
-        viewableLandmarksIdx = viewableLandmarksIdx & landmarks_cam(3,:) > 0;
+        %Determine which landmarks are viewable
+        isInFieldOfView = pixels(1,:) > 0 & pixels(1,:) < camRes(2) & pixels(2,:) > 0 & pixels(2,:) < camRes(1);
+        isInFieldOfView = isInFieldOfView & landmarks_cam(3,:) > 0;
         
         
         %viewableLandmarkIds = find(viewableLandmarksIdx);
         %viewableLandmarkIds = viewableLandmarkIds(:);
         %viewableLandmarks = landmarks_cam(:, viewableLandmarksIdx);
+        if ~isInFieldOfView
+            pixels = [-1; -1];
+            n_p = [0; 0];
+        else
+            n_p = simSetup.pixelNoiseStd*randn(2,size(pixels,2)); %image noise        
+        end
         
-        pixels(:, ~viewableLandmarksIdx) = -1;
-
-        %Generate pixel noise
-        n_p = simSetup.pixelNoiseStd*randn(2,size(pixelMeasurements,2)); %image noise        
-      
-        pixelMeasurements =  pixelMeasurements + n_p;
+        pixelMeasurements(:, step_i) =  pixels + n_p;
         
 end
 
--- Matlab/test/testGNest.m
+++ Matlab/test/testGNest.m
@@ -1,24 +1,19 @@
 %% Test feature Gauss Newton triangulation
 %Generate a forward trajectory
+addpath('../utils');
 i = 1;
 T_wCam_GT = [];
 for t = 0:0.2:2
     T_wCam_GT(:,:,i) = [eye(3) [t 0 0]'; 0 0 0 1];
     i=i+1;
 end
-%Generate the true landmarks
+%Generate the true location
 landmarks_w = [1 0 5]';
-%for x_i = 0:0.1:2
-%     for y_i = -1:0.1:1
-%         landmarks_w = [landmarks_w [x_i y_i 5]'];
-%     end
-% end
 simSetup.pixelNoiseStd = 1; %pixels
 simSetup.cameraResolution = [1280, 960]; %pixels
 
 %Set the camera intrinsics
-pixelWidth = 4.8*1e-6;
-focalLength = 4*1e-3/pixelWidth; 
+focalLength = 600; 
 c_u = 640;
 c_v = 480;
 
@@ -26,10 +21,19 @@ K  = [focalLength 0 c_u;
     0 focalLength c_v;
     0 0 1];
 
-invK = inv(K);
 
 imageMeasurements = genFeatureMeasurements(T_wCam_GT, landmarks_w, K, simSetup)
 
+observations = imageMeasurements;
+observations(1,:) = (observations(1,:) - c_u)/focalLength;
+observations(2,:) = (observations(2,:) - c_v)/focalLength;
+
+camStates = {};
+% Extract all viewable measurements
+for step_i = 1:size(T_wCam_GT,3)
+    camStates{step_i}.q_CG  = rotMatToQuat(T_wCam_GT(1:3,1:3)');
+    camStates{step_i}.p_C_G = T_wCam_GT(1:3,4);
+end
 
 
-%[p_f_G] = calcGNPosEst(camStates, observations, camera)
+[p_f_G] = calcGNPosEst(camStates, observations, {})
