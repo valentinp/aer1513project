222a6b0b0cfc401a304f85d7dd076f2443f1d515
Lee Clement <clement.leopold@gmail.com>
2014-12-05 18:26:36 -0500

    So many typos

--- Matlab/augmentState.m
+++ Matlab/augmentState.m
@@ -9,16 +9,16 @@ function msckfState_aug = augmentState(msckfState, camera)
     
     J = calcJ(camera, msckfState.imuState, msckfState.camStates);
     
-    N = size(msckfState.cameraStates,2);
+    N = size(msckfState.camStates,2);
     
-    tempMat = [eye(15+6*N); J];
+    tempMat = [eye(12+6*N); J];
     
     P_aug = tempMat * P * tempMat';
     
     msckfState_aug = msckfState;
     msckfState_aug.camStates{N+1}.p_C_G = p_C_G;
     msckfState_aug.camStates{N+1}.q_CG = q_CG;
-    msckfState_aug.imuCovar = P_aug(1:15,1:15);
-    msckfState_aug.camCovar = P_aug(16:end,16:end);
-    msckfState_aug.imuCamCovar = P_aug(1:15, 16:end);
+    msckfState_aug.imuCovar = P_aug(1:12,1:12);
+    msckfState_aug.camCovar = P_aug(13:end,13:end);
+    msckfState_aug.imuCamCovar = P_aug(1:12, 13:end);
 end
