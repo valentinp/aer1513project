373408f90d8fc371e40a167b1d65cc57f4abb90d
Lee Clement <clement.leopold@gmail.com>
2014-12-06 17:41:44 -0500

    Added some comments for clarity

--- Matlab/augmentState.m
+++ Matlab/augmentState.m
@@ -1,20 +1,27 @@
 function msckfState_aug = augmentState(msckfState, camera)
-    
+% Augments the MSCKF state with a new camera pose    
+
     C_IG = quatToRotMat(msckfState.imuState.q_IG);
+    
+    % Compute camera pose from current IMU pose
     q_CG = quatLeftComp(camera.q_CI) * msckfState.imuState.q_IG;
     p_C_G = msckfState.imuState.p_I_G + C_IG' * camera.p_C_I;
 
+    % Build MSCKF covariance matrix
     P = [msckfState.imuCovar, msckfState.imuCamCovar;
         msckfState.imuCamCovar', msckfState.camCovar];
     
+    % Camera state Jacobian
     J = calcJ(camera, msckfState.imuState, msckfState.camStates);
     
     N = size(msckfState.camStates,2);
     
     tempMat = [eye(12+6*N); J];
     
+    % Augment the MSCKF covariance matrix
     P_aug = tempMat * P * tempMat';
     
+    % Break everything into appropriate structs
     msckfState_aug = msckfState;
     msckfState_aug.camStates{N+1}.p_C_G = p_C_G;
     msckfState_aug.camStates{N+1}.q_CG = q_CG;
