397963c87e9c26b894af9290ae09d53c5f93eb43
Valentin Peretroukhin <valentinp@gmail.com>
2014-12-08 13:41:03 -0500

    Fixed Hoj to account for all camera observations

--- Matlab/calcHoj.m
+++ Matlab/calcHoj.m
@@ -1,30 +1,37 @@
-function [H_o_j, A_j] = calcHoj(p_f_G, msckfState, camStateIndex)
+function [H_o_j, A_j] = calcHoj(p_f_G, msckfState, camStateIndices)
 %CALCHOJ Calculates H_o_j according to Mourikis 2007
 % Inputs: p_f_G: feature location in the Global frame
 %         msckfState: the current window of states
 %         camStateIndex: i, with camState being the ith camera pose in the window       
 % Outputs: H_o_j, A
 
-
-camState = msckfState{camStateIndex + 1};
-N = length(msckfState);
-
-%The feature position in the camera frame
-p_f_C = quatToRotMat(camState.q_CG)*(p_f_G - camState.p_C_G);
-
-X = p_F_C(1);
-Y = p_F_C(2);
-Z = p_F_C(3);
-
-J_i = (1/Z)*[1 0 -X/Z; 0 1 -Y/Z];
-
-
+N = length(camStateIndices);
 H_x_j = zeros(2, 12 + 6*N);
-H_x_j(:,12+6*(camStateIndex-1) + 1:15+6*(camStateIndex-1) + 3) = J_i*crossMat(p_f_C);
-H_x_j(:,(12+6*(camStateIndex-1) + 4):(15+6*(camStateIndex-1) + 6)) = -J_i*quatToRotMat(camState.q_CG);
+H_f_j = zeros(2, 3*N);
 
+c_i = 1;
+for camStateIndex = camStateIndices
+    camState = msckfState{camStateIndex + 1};
 
-A_j = null(H_x_j);
-H_o_j = A'*H_x_j;
+    %The feature position in the camera frame
+    p_f_C = quatToRotMat(camState.q_CG)*(p_f_G - camState.p_C_G);
+
+    X = p_F_C(1);
+    Y = p_F_C(2);
+    Z = p_F_C(3);
+
+    J_i = (1/Z)*[1 0 -X/Z; 0 1 -Y/Z];
+
+    H_f_j(:, (3*c_i - 2):3*c_i) = J_i*quatToRotMat(camState.q_CG);
+
+    H_x_j(:,12+6*(camStateIndex-1) + 1:15+6*(camStateIndex-1) + 3) = J_i*crossMat(p_f_C);
+    H_x_j(:,(12+6*(camStateIndex-1) + 4):(15+6*(camStateIndex-1) + 6)) = -J_i*quatToRotMat(camState.q_CG);
+
+    c_i = c_i + 1;
+end
+
+A_j = null(H_f_j);
+H_o_j = A_j'*H_x_j;
+
 end
 
