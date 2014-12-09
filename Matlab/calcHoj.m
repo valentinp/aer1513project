a5e2890c255b4c89013b66f6a16284ba8d720feb
Valentin Peretroukhin <valentinp@gmail.com>
2014-12-09 14:58:54 -0500

    Working on Hfj nullspace

--- Matlab/calcHoj.m
+++ Matlab/calcHoj.m
@@ -11,21 +11,21 @@ H_f_j = zeros(2, 3*N);
 
 c_i = 1;
 for camStateIndex = camStateIndices
-    camState = msckfState{camStateIndex + 1};
+    camState = msckfState.camStates{camStateIndex};
 
     %The feature position in the camera frame
     p_f_C = quatToRotMat(camState.q_CG)*(p_f_G - camState.p_C_G);
 
-    X = p_F_C(1);
-    Y = p_F_C(2);
-    Z = p_F_C(3);
+    X = p_f_C(1);
+    Y = p_f_C(2);
+    Z = p_f_C(3);
 
     J_i = (1/Z)*[1 0 -X/Z; 0 1 -Y/Z];
 
     H_f_j(:, (3*c_i - 2):3*c_i) = J_i*quatToRotMat(camState.q_CG);
 
-    H_x_j(:,12+6*(camStateIndex-1) + 1:15+6*(camStateIndex-1) + 3) = J_i*crossMat(p_f_C);
-    H_x_j(:,(12+6*(camStateIndex-1) + 4):(15+6*(camStateIndex-1) + 6)) = -J_i*quatToRotMat(camState.q_CG);
+    H_x_j(:,12+6*(camStateIndex-1) + 1:12+6*(camStateIndex-1) + 3) = J_i*crossMat(p_f_C);
+    H_x_j(:,(12+6*(camStateIndex-1) + 4):(12+6*(camStateIndex-1) + 6)) = -J_i*quatToRotMat(camState.q_CG);
 
     c_i = c_i + 1;
 end
