7d6bb7ce169f4f6d6e2d6a0905913f8bfa5d1b27
Lee Clement <clement.leopold@gmail.com>
2014-12-28 23:54:19 -0600

    Gridlines yo

--- Matlab/plotting/nullspace_plots.m
+++ Matlab/plotting/nullspace_plots.m
@@ -9,6 +9,7 @@ swf_rot_err = theta_CG_err_NSToff;
 
 transLim = 0.4;
 rotLim = 0.4;
+fontSize = 14;
 
 figure
 subplot(3,1,1)
@@ -20,7 +21,8 @@ ylim([-transLim transLim])
 legend('With Nullspace Projection', 'Without Nullspace Projection');
 title(sprintf('Translational Error'))
 ylabel('\delta r_x [m]')
-
+set(gca,'FontSize',fontSize)
+grid on; grid minor;
 
 subplot(3,1,2)
 plot(tPlot, msckf_trans_err(2,:), '-b', 'LineWidth', 1.2)
@@ -29,6 +31,8 @@ plot(tPlot, swf_trans_err(2,:), '-r', 'LineWidth', 1.2)
 xlim([tPlot(1) tPlot(end)]);
 ylim([-transLim transLim])
 ylabel('\delta r_y [m]')
+set(gca,'FontSize',fontSize)
+grid on; grid minor;
 
 subplot(3,1,3)
 plot(tPlot, msckf_trans_err(3,:), '-b', 'LineWidth', 1.2)
@@ -40,6 +44,8 @@ ylabel('\delta r_z [m]')
 xlabel('t_k [s]')
 %set(gca,'FontSize',12)
 %set(findall(gcf,'type','text'),'FontSize',12)
+set(gca,'FontSize',fontSize)
+grid on; grid minor;
 
 figure
 subplot(3,1,1)
@@ -51,7 +57,8 @@ ylim([-rotLim rotLim])
 title(sprintf('Rotational Error'))
 ylabel('\delta\theta_x')
 legend('With Nullspace Projection', 'Without Nullspace Projection');
-
+set(gca,'FontSize',fontSize)
+grid on; grid minor;
 
  
 subplot(3,1,2)
@@ -61,6 +68,8 @@ plot(tPlot, swf_rot_err(3,:), '-r', 'LineWidth', 1.2)
 xlim([tPlot(1) tPlot(end)]);
 ylim([-rotLim rotLim])
 ylabel('\delta\theta_y')
+set(gca,'FontSize',fontSize)
+grid on; grid minor;
 
 subplot(3,1,3)
 plot(tPlot, msckf_rot_err(3,:), '-b', 'LineWidth', 1.2)
@@ -70,3 +79,5 @@ xlim([tPlot(1) tPlot(end)]);
 ylim([-rotLim rotLim])
 ylabel('\delta\theta_z')
 xlabel('t_k [s]')
+set(gca,'FontSize',fontSize)
+grid on; grid minor;
