ab4bb080ab3b94365e7dffd0fc563351ebbc26c7
Lee Clement <clement.leopold@gmail.com>
2014-12-30 10:46:30 -0600

    Experimental wordsmithing

--- Matlab/plotting/datasetPlot.m
+++ Matlab/plotting/datasetPlot.m
@@ -7,9 +7,12 @@ lineWidth = 1.2;
 
 figure(1); clf; hold on;
 
+% scatter3(rho_i_pj_i(1,1:40), rho_i_pj_i(2,1:40), rho_i_pj_i(3,1:40), markerSize, 'om', 'fill');
+% scatter3(rho_i_pj_i(1,41:60), rho_i_pj_i(2,41:60), rho_i_pj_i(3,41:60), markerSize, 'og', 'fill');
+% scatter3(rho_i_pj_i(1,61:end), rho_i_pj_i(2,61:end), rho_i_pj_i(3,61:end), markerSize, 'or', 'fill');
 scatter3(rho_i_pj_i(1,:), rho_i_pj_i(2,:), rho_i_pj_i(3,:), markerSize, 'or', 'fill');
 plot3(r_i_vk_i(1,:), r_i_vk_i(2,:), r_i_vk_i(3,:), '-b', 'LineWidth', lineWidth);
-legend('Features', 'Sensor trajectory');
+legend('Features (100)', 'Features (60)', 'Features (40)', 'Sensor trajectory');
 
 set(gca, 'FontSize', fontSize);
 campos([-18.7401, -5.8668, 10.6855]);
